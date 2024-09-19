0f6b689e5fe87e8e09e5350d3b61ee2ce193c8ec
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2024-09-19 19:42:43 +0200

    Add Activate

---
+++ Casks/Activate.rb
@@ -0,0 +1,19 @@
+cask "Activate" do
+  version "1.0.0"
+  sha256 "34248b16a5cbe9b9d800c86e83dbd7ce2e3285737df5df872a735e510c465a25"
+
+  url "https://github.com/Zarox28/Activate/releases/download/v#{version}/Activate.dmg"
+  name "Activate"
+  desc "Activate MacOS is a playful macOS app that mimics the Windows activation message."
+  homepage "https://github.com/Zarox28/Activate"
+
+  app "Activate.app"
+
+  zap trash: [
+    "~/Library/Application Support/Activate",
+    "~/Library/Preferences/com.Zarox28.Activate.plist",
+    "~/Library/Caches/com.Zarox28.Activate",
+    "~/Library/Logs/Activate",
+    "~/Library/Saved Application State/com.Zarox28.Activate.savedState"
+  ]
+end
